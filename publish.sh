14b40802dc00646ec38e257381b2f91b17a47db1
Dustin Collins <dustin.collins@cyberark.com>
2018-07-25 11:34:08 -0500

    Push to DockerHub cyberark/conjur-authn-k8s-client as well

--- publish.sh
+++ publish.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -e
 
-DOCKERHUB_IMAGE='cyberark/conjur-kubernetes-authenticator'
+DOCKERHUB_IMAGE_OLD='cyberark/conjur-kubernetes-authenticator'  # backwards-compatible image name
+DOCKERHUB_IMAGE_NEW='cyberark/conjur-authn-k8s-client'
 VERSION_TAG="$(<VERSION)"
 
 REDHAT_IMAGE='scan.connect.redhat.com/ospid-1c46a2de-1d88-40e6-a433-7114ad0099cb/conjur-openshift-authenticator-client'
 
-docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE"
-docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE:$VERSION_TAG"
+docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE_OLD"
+docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE_OLD:$VERSION_TAG"
+
+docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE_NEW"
+docker tag conjur-authn-k8s-client:dev "$DOCKERHUB_IMAGE_NEW:$VERSION_TAG"
+
+docker push $DOCKERHUB_IMAGE_OLD
+docker push "$DOCKERHUB_IMAGE_OLD:$VERSION_TAG"
+
+docker push $DOCKERHUB_IMAGE_NEW
+docker push "$DOCKERHUB_IMAGE_NEW:$VERSION_TAG"
 
 docker tag conjur-authn-k8s-client:dev-redhat "$REDHAT_IMAGE"
 docker tag conjur-authn-k8s-client:dev-redhat "$REDHAT_IMAGE:$VERSION_TAG"
-
-docker push $DOCKERHUB_IMAGE
-docker push "$DOCKERHUB_IMAGE:$VERSION_TAG"
 
 docker push "$REDHAT_IMAGE:$VERSION_TAG" || { echo 'Red Hat push FAILED!'; exit 0; }  # you can't push the same tag twice to redhat registry, so ignore errors
