f7e7e5f982f01b61cb004c0576c79e745226b4a5
orenbm <oren.benmeir@cyberark.com>
2019-08-14 12:06:56 +0300

    Retrieve k8s Secrets and update them with Conjur secrets
    This commit holds the main logic of handling k8s secrets. The flow is:
    1. Retrieve all the required secrets from k8s
    2. Parse each secret's "conjur-map" entry
    3. Retrieve required Conjur secrets and update them in the map
    4. Patch the k8s secrets with the values from Conjur
    
    This commit includes also:
    - Update Secrets config to read the K8S_SECRETS env var that list the required secrets for the pod
    - Remove logic & files of storing secrets in the volume

--- pkg/secrets/kubernetes_secrets.go
+++ pkg/secrets/kubernetes_secrets.go
@@ -2,54 +2,76 @@ package secrets
 
 import (
 	"fmt"
-
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-func storeSecretsInKubeSecrets(namespace string, secretName string, secrets []Secret) error {
+type K8sSecret struct {
+	Secret *v1.Secret
+}
 
-	// Create a map of secret key to value to place in the
-	// Kubernetes secret
-	secretsData := make(map[string][]byte)
-	for _, secret := range secrets {
-		secretsData[secret.Key] = secret.SecretBytes
+func retrieveK8sSecret(namespace string, secretName string) (*K8sSecret, error) {
+	// TODO: extract this to another function
+	// Create the Kubernetes client
+	InfoLogger.Print("Creating Kubernetes client...")
+	kubeConfig, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load in-cluster Kubernetes client config: %s", err)
 	}
+
+	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Kubernetes client: %s", err)
+	}
+
+	// TODO: extract until here
+
+	InfoLogger.Printf("Retrieving Kubernetes secret '%s' from namespace '%s'...", secretName, namespace)
+	k8sSecret, err := kubeClient.CoreV1().Secrets(namespace).Get(secretName, metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve Kubernetes secret: %s", err)
+	}
+
+	return &K8sSecret{
+		Secret: k8sSecret,
+	}, nil
+}
+
+func patchK8sSecret(namespace string, secretName string, stringDataEntriesMap map[string][]byte) error {
+	// TODO: extract this to another function
 
 	// Create the Kubernetes client
 	InfoLogger.Print("Creating Kubernetes client...")
 	kubeConfig, err := rest.InClusterConfig()
 	if err != nil {
-		return fmt.Errorf("Failed to load in-cluster Kubernetes client config: %s", err)
+		return fmt.Errorf("failed to load in-cluster Kubernetes client config: %s", err)
 	}
 
 	kubeClient, err := kubernetes.NewForConfig(kubeConfig)
 	if err != nil {
-		return fmt.Errorf("Failed to create Kubernetes client: %s", err)
+		return fmt.Errorf("failed to create Kubernetes client: %s", err)
 	}
 
-	// Create the Kubernetes secret
-	secret := v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: secretName,
-		},
-		Data: secretsData,
+	// TODO: extract until here
+
+	// TODO: improve this code
+	data := []byte("{\"stringData\":{")
+	for key, value := range stringDataEntriesMap {
+		entry := []byte(fmt.Sprintf("\"%s\": \"", key))
+		entry = append(append(entry, value...), []byte("\"")...)
+		entry = append(entry, []byte(",")...)
+		data = append(data, entry...)
 	}
+	// TODO: remove last comma
+	data = append(data, []byte("}}")...)
 
-	// Store the secret in Kubernetes
-	InfoLogger.Printf("Creating the Kubernetes secret '%s' in namespace '%s'...", secretName, namespace)
-	_, err = kubeClient.CoreV1().Secrets(namespace).Create(&secret)
-
-	if errors.IsAlreadyExists(err) {
-		err = nil
-		_, err = kubeClient.CoreV1().Secrets(namespace).Update(&secret)
-	}
-
+	InfoLogger.Printf("Patching Kubernetes secret '%s' in namespace '%s'...", secretName, namespace)
+	_, err = kubeClient.CoreV1().Secrets(namespace).Patch(secretName, types.StrategicMergePatchType, data)
 	if err != nil {
-		return fmt.Errorf("Failed to write Kubernetes secret: %s", err)
+		return fmt.Errorf("failed to patch Kubernetes secret: %s", err)
 	}
 
 	return nil
