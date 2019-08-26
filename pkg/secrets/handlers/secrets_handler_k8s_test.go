8b1fa599aa4030293c98cc2a24bbd549f3888bfc
Sigal Sax <ssax18@gmail.com>
2019-08-26 16:41:33 +0300

    Add UTs to secrets handler This adds UTs to secrets handler to check that the variable ID is replaced with secret value and that the byte arrya is cleared from memory after use

--- pkg/secrets/handlers/secrets_handler_k8s_test.go
+++ pkg/secrets/handlers/secrets_handler_k8s_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/cyberark/conjur-authn-k8s-client/pkg/secrets/k8s"
 	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
 	"sort"
@@ -31,6 +32,37 @@ func TestSecretsHandlerK8sUseCase(t *testing.T) {
 			_, err := getVariableIDsToRetrieve(m)
 
 			So(err.Error(), ShouldEqual, "error map should not be empty")
+		})
+	})
+
+	Convey("updateK8sSecretsMapWithConjurSecrets", t, func () {
+		secret := []byte{'s', 'u', 'p', 'e', 'r'}
+		conjurSecrets := make(map[string][]byte)
+		conjurSecrets["account:variable:allowed/username"] = secret
+
+		newDataEntriesMap := make(map[string][]byte)
+		newDataEntriesMap["username"] = []byte("allowed/username")
+
+		k8sSecretsMap := make(map[string]map[string][]byte)
+		k8sSecretsMap["mysecret"] = newDataEntriesMap
+
+		pathMap := make(map[string]string)
+		pathMap["allowed/username"] = "mysecret:username"
+
+		k8sSecretsStruct := k8s.K8sSecretsMap{k8sSecretsMap, pathMap}
+		err := updateK8sSecretsMapWithConjurSecrets(&k8sSecretsStruct, conjurSecrets)
+
+		eq := reflect.DeepEqual(k8sSecretsStruct.K8sSecrets["mysecret"]["username"], secret)
+		Convey("Returns true if K8sSecretsMap's variable ID is replaced with corresponding secret value", func() {
+			So(err, ShouldEqual, nil)
+			So(eq, ShouldEqual, true)
+		})
+
+		Convey("Returns true if Conjur secret byte array is cleared from memory", func() {
+			// secret value should be cleared from memory after use
+			empty := make([]byte, len(secret))
+			eq = reflect.DeepEqual(conjurSecrets["account:variable:allowed/username"], empty)
+			So(eq, ShouldEqual, true)
 		})
 	})
 }
