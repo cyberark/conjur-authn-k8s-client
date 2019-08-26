3156d91259f898c6376fb55fbae10090734c0ce7
orenbm <oren.benmeir@cyberark.com>
2019-08-26 11:35:23 +0300

    Moved test file as it test a method that was moved to another class

--- pkg/secrets/handlers/secrets_handler_k8s_test.go
+++ pkg/secrets/handlers/secrets_handler_k8s_test.go
@@ -1,13 +1,12 @@
-package conjur
+package handlers
 
 import (
-	"github.com/cyberark/conjur-authn-k8s-client/pkg/secrets"
 	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
 	"testing"
 )
 
-func TestConjurSecrets(t *testing.T) {
+func TestSecretsHandlerK8sUseCase(t *testing.T) {
 	Convey("getVariableIDsToRetrieve", t, func() {
 		Convey("Returns true if pathMap output ia as expected", func() {
 			m := make(map[string]string)
@@ -15,7 +14,7 @@ func TestConjurSecrets(t *testing.T) {
 			m["account/var_path1"] = "secret1:key1"
 			m["account/var_path2"] = "secret1:key2"
 			variableIDsExpected := []string{"account/var_path1", "account/var_path2"}
-			variableIDsActual, err := secrets.getVariableIDsToRetrieve(m)
+			variableIDsActual, err := getVariableIDsToRetrieve(m)
 
 			eq := reflect.DeepEqual(variableIDsActual, variableIDsExpected)
 
@@ -25,7 +24,7 @@ func TestConjurSecrets(t *testing.T) {
 
 		Convey("Returns error if map input is empty", func() {
 			m := make(map[string]string)
-			_, err := secrets.getVariableIDsToRetrieve(m)
+			_, err := getVariableIDsToRetrieve(m)
 
 			So(err.Error(), ShouldEqual, "error map should not be empty")
 		})
