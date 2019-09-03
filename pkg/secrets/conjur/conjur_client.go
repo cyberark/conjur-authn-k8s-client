91273f59a6467feaf1bcb73e395318b1dea4a5b5
orenbm <oren.benmeir@cyberark.com>
2019-09-03 12:05:02 +0300

    ConjurClient interface refactor
    - Renamed ConjurProvider to ConjurClient
    - Moved the interface to conjur_client.go and added a description

--- pkg/secrets/conjur/conjur_client.go
+++ pkg/secrets/conjur/conjur_client.go
@@ -6,7 +6,17 @@ import (
 	log "github.com/cyberark/conjur-authn-k8s-client/pkg/logging"
 )
 
-func conjurProvider(tokenData []byte) (ConjurProvider, error) {
+/*
+	Client for communication with Conjur. In this project it is used only for
+    batch secrets retrieval so we expose only this method of the client.
+
+	The name ConjurClient also improves readability as Client can be ambiguous.
+*/
+type ConjurClient interface {
+	RetrieveBatchSecrets([]string) (map[string][]byte, error)
+}
+
+func NewConjurClient(tokenData []byte) (ConjurClient, error) {
 	log.InfoLogger.Printf(log.CAKC015I)
 	config, err := conjurapi.LoadConfig()
 	if err != nil {
--- pkg/secrets/conjur/conjur_secrets_fetcher.go
+++ pkg/secrets/conjur/conjur_secrets_fetcher.go
@@ -4,11 +4,7 @@ import (
 	log "github.com/cyberark/conjur-authn-k8s-client/pkg/logging"
 )
 
-type ConjurProvider interface {
-	RetrieveBatchSecrets([]string) (map[string][]byte, error)
-	RetrieveSecret(string) ([]byte, error)
-}
-
+// We have this interface so we can mock it in UTs
 type ConjurSecretsFetcherInterface interface {
 	// functions will need to be implemented by another struct
 	RetrieveConjurSecrets(accessToken []byte, variableIDs []string) (map[string][]byte, error)
@@ -19,18 +15,18 @@ type ConjurSecretsFetcher struct{}
 
 func (conjurSecretsFetcher ConjurSecretsFetcher) RetrieveConjurSecrets(accessToken []byte, variableIDs []string) (map[string][]byte, error) {
 	var (
-		provider ConjurProvider
-		err      error
+		conjurClient ConjurClient
+		err          error
 	)
 
 	log.InfoLogger.Println(log.CAKC018I, variableIDs)
 
-	provider, err = conjurProvider(accessToken)
+	conjurClient, err = NewConjurClient(accessToken)
 	if err != nil {
 		return nil, log.PrintAndReturnError(log.CAKC020E)
 	}
 
-	retrievedSecrets, err := provider.RetrieveBatchSecrets(variableIDs)
+	retrievedSecrets, err := conjurClient.RetrieveBatchSecrets(variableIDs)
 	if err != nil {
 		return nil, log.PrintAndReturnError(log.CAKC021E, err.Error())
 	}
