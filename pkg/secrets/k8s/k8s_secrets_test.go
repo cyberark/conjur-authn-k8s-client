bf422d12ddc33b88a6f31fde266d6ec479e4473a
Sigal Sax <ssax18@gmail.com>
2019-09-03 17:36:33 +0300

    Add UTs for checking escape characters

--- pkg/secrets/k8s/k8s_secrets_test.go
+++ pkg/secrets/k8s/k8s_secrets_test.go
@@ -1,13 +1,13 @@
 package k8s
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
 	log "github.com/cyberark/conjur-authn-k8s-client/pkg/logging"
+	. "github.com/smartystreets/goconvey/convey"
 	"reflect"
-	"testing"
 	"regexp"
 	"sort"
 	"strings"
+	"testing"
 )
 
 func TestKubernetesSecrets(t *testing.T) {
@@ -37,6 +37,26 @@ func TestKubernetesSecrets(t *testing.T) {
 				stringDataEntryExpectedSorted := strings.Split(match[1], ",")
 				sort.Strings(stringDataEntryExpectedSorted)
 				eq := reflect.DeepEqual(stringDataEntryActualSorted, stringDataEntryExpectedSorted)
+				So(eq, ShouldEqual, true)
+			})
+		})
+
+		Convey("Given a map of data entries and a secret with backslashes", func() {
+			m := make(map[string][]byte)
+			// need to simulate a conjur secret, with 1 backslash, so we use unicode to simulate this
+			m["user"] = []byte("super\u005csecret");
+			DataEntry, err := generateStringDataEntry(m);
+
+			Convey("Finishes without raising an error", func() {
+				So(err, ShouldEqual, nil)
+			})
+
+			Convey("Returns proper password with escaped characters", func() {
+				// before sending secret to k8s there are two backslashes
+				// k8s cuts off the second backslash so here we are expecting two
+				stringDataEntryExpected := `{"stringData":{"user":"super\\secret"}}`
+				stringDataEntryActual := string(DataEntry)
+				eq := reflect.DeepEqual(stringDataEntryActual, stringDataEntryExpected)
 				So(eq, ShouldEqual, true)
 			})
 		})
