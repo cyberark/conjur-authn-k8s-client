f3f6d0216249b125630de45784b72f11b74aa271
Oren Ben Meir <oren.benmeir@cyberark.com>
2020-09-13 13:39:00 +0300

    Extract env var value read into a util (#160)
    * Extract env var value read into a util
    
    We would like to better UT the logic of reading a value from the env
    and defaulting to a default value if it's not present. We also have the
    logic that transforms this env var value into an int or a duration (and
    maybe more in the future) and it is best that this logic is UTed and is
    implemented in its own util. This improves the readability of the
    config.go file and of the util functions themselves.
    
    This will also let us use these utils in other projects that consume the
    authn-client without implementing them there as well.
    
    * Give example for CONJUR_TOKEN_TIMEOUT in README

---
+++ pkg/utils/environment_variable.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/cyberark/conjur-authn-k8s-client/pkg/log"
+)
+
+// GetEnvFunc is a function that returns a value for an environment variable
+type GetEnvFunc func(envVarName string) string
+
+// defaultGetEnv is the default GetEnvFunc. It uses the standard OS Getenv.
+var defaultGetEnvFunc = os.Getenv
+
+// IntFromEnvOrDefault extracts the value of the given environment variable,
+// or the default value if the environment variable is not defined, and returns
+// it as an int object
+func IntFromEnvOrDefault(
+	envVarName string,
+	defaultValue string,
+	getEnv GetEnvFunc,
+) (int, error) {
+	value := envVarValueOrDefault(envVarName, defaultValue, getEnv)
+	valueInt, err := strconv.Atoi(value)
+	if err != nil {
+		return 0, log.RecordedError(log.CAKC010E, envVarName, err.Error())
+	}
+
+	return valueInt, nil
+}
+
+// DurationFromEnvOrDefault extracts the value of the given environment variable,
+// or the default value if the environment variable is not defined, and returns
+// it as a time.Duration object
+func DurationFromEnvOrDefault(
+	envVarName string,
+	defaultValue string,
+	getEnv GetEnvFunc,
+) (time.Duration, error) {
+	value := envVarValueOrDefault(envVarName, defaultValue, getEnv)
+	valueDuration, err := time.ParseDuration(value)
+	if err != nil {
+		return 0, log.RecordedError(log.CAKC010E, envVarName, err.Error())
+	}
+
+	return valueDuration, nil
+}
+
+func envVarValueOrDefault(envVarName string, defaultValue string, getEnv GetEnvFunc) string {
+	if getEnv == nil {
+		getEnv = defaultGetEnvFunc
+	}
+	value := getEnv(envVarName)
+	if len(value) > 0 {
+		return value
+	}
+
+	return defaultValue
+}
