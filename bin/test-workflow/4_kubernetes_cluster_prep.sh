66531f2a462d8d27631591adf0ad86a76db44814
Samir Shetty <samir.shetty@cyberark.com>
2021-05-26 18:50:40 -0700

    Remove helm uninstalls where possible

--- bin/test-workflow/4_kubernetes_cluster_prep.sh
+++ bin/test-workflow/4_kubernetes_cluster_prep.sh
@@ -8,13 +8,9 @@ set_namespace default
 # Prepare our cluster with conjur and authnK8s credentials in a golden configmap
 announce "Installing cluster prep chart"
 pushd $(dirname "$0")/../../helm/kubernetes-cluster-prep > /dev/null
-    if [ "$(helm list -q -n $CONJUR_NAMESPACE | grep "^cluster-prep$")" = "cluster-prep" ]; then
-        helm uninstall cluster-prep -n "$CONJUR_NAMESPACE"
-    fi
-
     ./bin/get-conjur-cert.sh -v -i -u "$CONJUR_APPLIANCE_URL"
 
-    helm install cluster-prep . -n "$CONJUR_NAMESPACE"  --debug --wait \
+    helm upgrade --install cluster-prep . -n "$CONJUR_NAMESPACE"  --debug --wait \
         --set conjur.account="$CONJUR_ACCOUNT" \
         --set conjur.applianceUrl="$CONJUR_APPLIANCE_URL" \
         --set conjur.certificateFilePath="files/conjur-cert.pem" \
--- bin/test-workflow/5_app_namespace_prep.sh
+++ bin/test-workflow/5_app_namespace_prep.sh
@@ -8,12 +8,8 @@ set_namespace default
 # Prepare a given namespace with a subset of credentials from the golden configmap
 announce "Installing application namespace prep chart"
 pushd $(dirname "$0")/../../helm/application-namespace-prep > /dev/null
-    if [ "$(helm list -q -n $TEST_APP_NAMESPACE_NAME | grep "^namespace-prep$")" = "namespace-prep" ]; then
-        helm uninstall namespace-prep -n "$TEST_APP_NAMESPACE_NAME"
-    fi
-
     # Namespace $TEST_APP_NAMESPACE_NAME will be created if it does not exist
-    helm install namespace-prep . -n "$TEST_APP_NAMESPACE_NAME"  --debug --wait \
+    helm upgrade --install namespace-prep . -n "$TEST_APP_NAMESPACE_NAME"  --debug --wait \
         --create-namespace \
         --set authnK8s.goldenConfigMap="authn-k8s-configmap" \
         --set authnK8s.namespace="$CONJUR_NAMESPACE" \
