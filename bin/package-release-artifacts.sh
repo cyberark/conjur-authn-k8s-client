aa2f46112615e29773cc4e92b3b62575f7d4414a
Shlomo Heigh <shlomo.heigh@cyberark.com>
2021-10-07 11:12:35 -0400

    Generate release file as single archive

--- bin/package-release-artifacts.sh
+++ bin/package-release-artifacts.sh
@@ -15,9 +15,10 @@ pushd helm > /dev/null
     mv $chart/generated/$chart.yaml $chart/generated/$chart-$version.yaml
   done
 
-  zip -v -j conjur-config-raw-k8s-manifests.zip \
-    conjur-config-cluster-prep/generated/* \
-    conjur-config-namespace-prep/generated/*
+  mkdir -p artifacts/raw-k8s-manifests
+  cp conjur-config-cluster-prep/generated/* \
+    conjur-config-namespace-prep/generated/* \
+    artifacts/raw-k8s-manifests/
 
   rm -r conjur-config-*-prep/generated
 
@@ -26,9 +27,19 @@ pushd helm > /dev/null
     helm package $chart
   done
 
-  mkdir -p artifacts
-  mv *.tgz *.zip -t artifacts
+  mkdir -p artifacts/helm-charts/
+  mv *.tgz artifacts/helm-charts/
   cp conjur-config-cluster-prep/bin/get-conjur-cert.sh artifacts/
+
+  # combine all the artifacts into a single archive file
+  # use the repo name and version in the archive filename
+  readonly REPO_NAME="${GITHUB_REPOSITORY##*/}"
+  readonly TAG_NAME="${GITHUB_REF##*/}"
+  readonly ARCHIVE_FILENAME="$REPO_NAME-$TAG_NAME.tar.gz"
+  tar -czf $ARCHIVE_FILENAME artifacts
+
+  rm -r artifacts/*
+  mv $ARCHIVE_FILENAME artifacts/
 
   # cleanup
   for chart in "${HELM_CHARTS[@]}"; do
